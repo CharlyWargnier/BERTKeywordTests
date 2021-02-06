c4a47817cebb07006e033b301436d8f21e4600be
Charly Wargnier <cwar05@gmail.com>
2021-02-06 12:00:30 +0000

    more text

--- app.py
+++ app.py
@@ -3,6 +3,26 @@ import streamlit as st
 
 doc = st.text_area('Area for textual entry')
 
+with st.beta_expander('Test text from post'):
+    '''
+        Supervised learning is the machine learning task of 
+         learning a function that maps an input to an output based 
+         on example input-output pairs.[1] It infers a function 
+         from labeled training data consisting of a set of 
+         training examples.[2] In supervised learning, each 
+         example is a pair consisting of an input object 
+         (typically a vector) and a desired output value (also 
+         called the supervisory signal). A supervised learning 
+         algorithm analyzes the training data and produces an 
+         inferred function, which can be used for mapping new 
+         examples. An optimal scenario will allow for the algorithm 
+         to correctly determine the class labels for unseen 
+         instances. This requires the learning algorithm to  
+         generalize from the training data to unseen situations 
+         in a 'reasonable' way (see inductive bias).
+
+    '''
+
 with st.beta_expander('Test text 01'):
     '''
     Next, we convert both the document as well as the candidate keywords/keyphrases to numerical data. We use BERT for this purpose as it has shown great results for both similarity- and paraphrasing tasks.
